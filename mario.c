9405dd6633eea99baadaeced3fa84e29fdb3372c
pacihub <pacihub@users.noreply.github.com>
2020-06-11 05:28:45 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -5,42 +5,32 @@ int main (void)
 
 {
     int height;
-
-    //do while loop to promp the user for values only within 1 to 8 range. The condition in the WHILE is the stuff that we do NOT want. false condition).
-   do
+    
+   do 
    {
-    height = get_int("Height:");
-   } while ( height >8 || height <1);
-
+      height = get_int("Height:");
+   } 
+   while (height > 8 || height < 1);
+ 
    {
-     // the for loop for height(number of rows)
-        for (int i = 1; i <= height; i++)
+     
+        for (int i = 1; i <= height; i++)   // the for loop for height(number of rows)
         {
-            //inner for loop to type spaces. compares with formula number of rows - 1. for example on row 1 out of 4 we will have 4-1 spaces.
-            for (int space = 1; space<=height -i ; space++)
+            for (int space = 1; space <= height - i ; space++)  //inner for loop for printing spaces
             {
                  printf(" ");
             }
-                //inner nested loop to type #/ compares agains the current iteration for row in the biggest for loop.
-                for(int hash = 1; hash<= i; hash++)
-                {
-                    printf("#");
-                }
                 
                 for(int hash = 1; hash<= i; hash++)
                 {
                     printf("#");
                 }
-                for (int space = 1; space<=height -i ; space++)
-            {
-                 printf(" ");
-            }
-
-
+                
+            
         printf("\n");
-
+        
         }
    }
-
-
-}
+  
+    
+}    
