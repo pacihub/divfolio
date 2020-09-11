3df541284b3bbca3c9b88496ba58e1af5f87a50b
pacihub <pacihub@users.noreply.github.com>
2020-09-11 13:53:20 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -5,32 +5,32 @@ int main(void)
 
 {
     int height;  //height of the pyramid
-    
-    do 
+
+    do
     {
         height = get_int("Height:");   //promting the user for height
-    } 
+    }
     while (height > 8 || height < 1);
- 
+
     {
-     
+
         for (int i = 1; i <= height; i++)   // the for loop for height(number of rows)
         {
-            for (int space = 1; space <= height - i ; space++)  
+            for (int space = 1; space <= height - i ; space++)
             {
                 printf(" ");
             }
-                
+
             for (int hash = 1; hash <= i; hash++)    //inner nested for loop for printing spaces
             {
                 printf("#");
             }
-                
-            
+
+
             printf("\n");
-        
+
         }
     }
-  
-    
-}    
+
+
+}
